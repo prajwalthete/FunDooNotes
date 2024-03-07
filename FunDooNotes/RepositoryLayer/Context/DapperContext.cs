5caa489b40d64873d976106ced5c5520b9df00bd
prajwal <prajwalthete98@gmail.com>
2024-03-07 15:58:57 +0530

    [Prajwal] Add : getting started with fundoonotes

---
+++ FunDooNotes/RepositoryLayer/Context/DapperContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace RepositoryLayer.Context
+{
+    public class DapperContext
+    {
+        private readonly IConfiguration _configuration;
+
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("FunDooConnection");
+        }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+
+    }
+}
